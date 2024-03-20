bd18c8e6133a1dbf6ecc53a3096161492ee3b961
grozzzny <grozzzny@gmail.com>
2024-03-20 18:21:26 +0200

    FIX(CAT-17): refactoring page profile

--- packages/client/src/components/input/input.tsx
+++ packages/client/src/components/input/input.tsx
@@ -1,46 +1,33 @@
 import { Input as AntInput, InputProps as AntInputProps } from 'antd';
 import './input.css';
+import React from 'react';
+import { TextAreaProps } from 'antd/es/input/TextArea';
 
 interface InputProps extends AntInputProps {
   type?: InputType;
 }
 
-type TextAreaProps = HTMLTextAreaElement;
-
 type InputType = 'password' | 'text' | 'textarea';
 
-export const Input = (props: InputProps | TextAreaProps) => {
+export const Input: React.FC<InputProps> = props => {
   const { type, ...restProps } = props;
-  let inputProps = {};
-
-  /* eslint-disable indent */
-  switch (type) {
-    case 'password':
-    case 'text':
-      inputProps = { ...(restProps as InputProps), className: 'input' };
-      break;
-    case 'textarea':
-      inputProps = { ...(restProps as TextAreaProps), className: 'textarea' };
-      break;
-    default:
-      inputProps = { ...(restProps as InputProps), className: 'input' };
-      break;
-  }
 
   if (type === 'text') {
-    return <AntInput {...inputProps} />;
+    return <AntInput className='input' {...restProps} />;
   }
 
   if (type === 'password') {
-    return <AntInput.Password {...inputProps} />;
+    return <AntInput.Password className='input' {...restProps} />;
   }
 
   if (type === 'textarea') {
-    const { TextArea } = AntInput;
-    return <TextArea {...inputProps} />;
+    return (
+      <AntInput.TextArea
+        className='textarea'
+        {...(restProps as TextAreaProps)}
+      />
+    );
   }
 
-  // Можно добавлять другие типы инпутов, если потребуется
-
-  return <AntInput {...inputProps} />;
+  return <AntInput className='input' {...restProps} />;
 };
