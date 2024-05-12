942656d97f24cafc5845fb5b7076ff7a8f73097c
grozzzny <grozzzny@gmail.com>
2024-05-12 15:26:33 +0200

    fix(CAT-54): tests

--- packages/client/src/pages/game-page/game-page.test.tsx
+++ packages/client/src/pages/game-page/game-page.test.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { fireEvent, render, waitFor } from '@testing-library/react';
-import { GamePage } from '@/pages';
+import { GamePage, LoginPage } from '@/pages';
 import '@testing-library/jest-dom';
+import store from '@/store';
+import { Provider } from 'react-redux';
 
 describe('Тестируем страницу GamePage в обход Auth Route HOC.', () => {
   test('проверяем рендер основного состояния страницы GameStartScreen с выбором уровня сложности', async () => {
-    const { getByText } = render(<GamePage />);
+    const { getByText } = render(
+      <Provider store={store}>
+        <GamePage />
+      </Provider>
+    );
     const chooseDifficultyElement = getByText(/Выберите уровень сложности/i);
     expect(chooseDifficultyElement).toBeInTheDocument();
   });
 
   test('проверяем отображение экрана начала игры после нажатия на кнопку "Start Game" на странице', async () => {
-    const { getByText, container } = render(<GamePage />);
+    const { getByText, container } = render(
+      <Provider store={store}>
+        <GamePage />
+      </Provider>
+    );
     const startGameButton = getByText('Играть!');
     fireEvent.click(startGameButton);
 
